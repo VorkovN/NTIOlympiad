04d779985406e14945bbeb11a3f8791ab07088ff
Edward Akhmetgaliev <edwardpro9@gmail.com>
2021-02-04 01:44:53 +0300

    Update inventarization

--- AENS_Robotech_best.py
+++ AENS_Robotech_best.py
@@ -15,31 +15,43 @@ cur_col = ""
 file = open("results.txt", "w")
 arr = [[2.0, 2.0], [2.0, 5.0]]
 colours = ["", "", "", ""]
-blue = 0
-red = 0
-green = 0
-yellow = 0
 
+cargo_amount = {
+    'blue': 0,
+    'red': 0,
+    'green': 0,
+    'yellow': 0
+}
+
+cur_cargo_amount = {
+    'blue': 0,
+    'red': 0,
+    'green': 0,
+    'yellow': 0
+}
+
+ALTITUDE = 1.7
+INVENTARIZATION_ALTITUDE = 0.5
+INVENTARIZATION_ENABLE = False
+INVENTARIZATION_CROP_IMAGE = 5
+INVENTARIZATION_TIME = 2
 AREA_TOLERANCE = 50
-SPEED = 0.3
+SPEED = 0.5
 DIR_PATH = 'AENS_Robotech/'
 
 bridge = CvBridge()
 
-rospy.init_node('task2')
+rospy.init_node('AENS_Robotech')
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 land = rospy.ServiceProxy('land', Trigger)
 
 
 def scan_cargo(img):
-    global blue
-    global green
-    global yellow
-    global red
+    global cur_cargo_amount
 
     blur = cv2.GaussianBlur(img, (5, 5), 0)
-    
+
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
     lower_green = np.array([50, 70, 90])
@@ -63,8 +75,8 @@ def scan_cargo(img):
         cv2.drawContours(img,[box],0,(0,255,0),-1) # рисуем прямоугольник
     #print ("green: " + str(green_count))
 
-    if green_count > green:
-        green = green_count
+    if green_count > cur_cargo_amount['green']:
+        cur_cargo_amount['green'] = green_count
 
 
     lower_yellow = np.array([25, 70, 90])
@@ -88,8 +100,8 @@ def scan_cargo(img):
         cv2.drawContours(img,[box],0,(0,255,255),-1) # рисуем прямоугольник
     #print ("yellow: " + str(yellow_count))
 
-    if yellow_count > yellow:
-        yellow = yellow_count
+    if yellow_count > cur_cargo_amount['yellow']:
+        cur_cargo_amount['yellow'] = yellow_count
 
     lower_blue = np.array([95, 70, 90])
     upper_blue = np.array([145, 250, 240])
@@ -112,8 +124,8 @@ def scan_cargo(img):
         cv2.drawContours(img,[box],0,(255,0,0),-1) # рисуем прямоугольник
     #print ("blue: " + str(blue_count))
 
-    if blue_count > blue:
-        blue = blue_count
+    if blue_count > cur_cargo_amount['blue']:
+        cur_cargo_amount['blue'] = blue_count
 
 
     lower_red1 = np.array([0, 70, 90])
@@ -143,15 +155,15 @@ def scan_cargo(img):
         box = np.int0(box) # округление координат
         cv2.drawContours(img,[box],0,(0,0,255),-1) # рисуем прямоугольник
 
-    if red_count > red:
-        red = red_count
+    if red_count > cur_cargo_amount['red']:
+        cur_cargo_amount['red'] = red_count
     return img
     #print ("red: " + str(red_count))
 
 
-def navigate_wait(x=0.0, y=0.0, z=1.7, yaw=float('nan'), speed=SPEED, frame_id='aruco_map', auto_arm=False, tolerance=0.2):
+def navigate_wait(x=0.0, y=0.0, z=ALTITUDE, yaw=float('nan'), speed=SPEED, frame_id='aruco_map', auto_arm=False, tolerance=0.2):
     navigate(x=x, y=y, z=z, yaw=yaw,
-	         speed=speed, frame_id=frame_id, auto_arm=auto_arm)
+                speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
     while not rospy.is_shutdown():
         telem = get_telemetry(frame_id='navigate_target')
@@ -165,70 +177,73 @@ def land_wait():
         rospy.sleep(0.1)
 
 def take_off():
-	navigate_wait(x=0, y=0, z=1.7, yaw=0.0, speed=SPEED, frame_id='body',  auto_arm=True, tolerance=0.1)
-	rospy.sleep(0.5)	
+    navigate_wait(x=0, y=0, z=ALTITUDE, yaw=0.0, speed=SPEED, frame_id='body',  auto_arm=True, tolerance=0.1)
+    rospy.sleep(0.5)
 
-def nextMark(xc, yc):
-	navigate_wait(x=float(xc), y=float(yc), z=1.7, yaw=0.0, speed=SPEED, frame_id='aruco_map',  auto_arm=True, tolerance=0.1)
-	rospy.sleep(1)
+def nextMark(xc, yc, zc=ALTITUDE):
+    navigate_wait(x=float(xc), y=float(yc), z=float(zc), yaw=0.0, speed=SPEED, frame_id='aruco_map',  auto_arm=True, tolerance=0.1)
+    rospy.sleep(0.5)
 
 def home():
-	navigate_wait(x=0.0, y=0.0, z=1.7, yaw=0.0, speed=SPEED, frame_id='aruco_map',  auto_arm=True, tolerance=0.1)
+    navigate_wait(x=0.0, y=0.0, z=ALTITUDE, yaw=0.0, speed=SPEED, frame_id='aruco_map',  auto_arm=True, tolerance=0.1)
     navigate_wait(x=0, y=0, z=0.5, yaw=0.0, speed=SPEED/2, frame_id='aruco_map',  auto_arm=True, tolerance=0.05)
-	rospy.sleep(1)
+    rospy.sleep(0.5)
 
-def image_callback(data):
-    global cur_col
-    global blue
-    global red
-    global green
-    global yellow
-    global callback_scan
-
+def image_callback(data):    
     cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')
-    cnt_img = scan_cargo(cv_image)
     cv2.imwrite(DIR_PATH+str(time.time())+".jpg", cv_image)
-    cv2.imwrite(DIR_PATH+str(time.time())+"contours.jpg", cnt_img)
+
+    if INVENTARIZATION_ENABLE:
+        height = cv_image.shape[0]
+        min_height, max_height = height//2 - height//(INVENTARIZATION_CROP_IMAGE*2),\
+                                 height//2 + height//(INVENTARIZATION_CROP_IMAGE*2)
+        cv_image = cv_image[min_height:max_height, :]
+        cnt_img = scan_cargo(cv_image)
+        cv2.imwrite(DIR_PATH+str(time.time())+"contours.jpg", cnt_img)
 
 def print_inventarization():
-    global cur_col
-    global blue
-    global red
-    global green
-    global yellow
-    global callback_scan
-
-    total = green + blue + red + yellow
+    total = sum(cargo_amount.values())
 
     print 'Balance', total, 'cargo'
-    print 'Type 0:', yellow, 'cargo'
-    print 'Type 1:', green, 'cargo'
-    print 'Type 2:', blue, 'cargo'
-    print 'Type 3:', red, 'cargo'
-
-	
+    print 'Type 0:', cargo_amount['yellow'], 'cargo'
+    print 'Type 1:', cargo_amount['green'], 'cargo'
+    print 'Type 2:', cargo_amount['blue'], 'cargo'
+    print 'Type 3:', cargo_amount['red'], 'cargo'
+  
 def main():
+    global cargo_amount, cur_cargo_amount, INVENTARIZATION_ENABLE
+
     if not os.path.isdir(DIR_PATH):
         os.mkdir(DIR_PATH)
 
     take_off()
     nextMark(0.0, 0.0)
-    nextMark(1.8, 0.0)
-    nextMark(1.8, 4.5)
+    nextMark(0.0, 4.95)
 
     image_sub = rospy.Subscriber('main_camera/image_raw_throttled', Image, image_callback, queue_size=1)
-    time.sleep(2)
+    for i in range(9):
+        nextMark(0.45*i, 4.95, INVENTARIZATION_ALTITUDE)
+        print 'INV on'
+        INVENTARIZATION_ENABLE = True
+        time.sleep(INVENTARIZATION_TIME)
+        INVENTARIZATION_ENABLE = False
+        print 'INV off'
+
+        for k,v in cur_cargo_amount.iteritems():
+            cargo_amount[k] += v
+            cur_cargo_amount[k] = 0
+        print_inventarization()
+        
+    time.sleep(1)
     print_inventarization()
     image_sub.unregister()
+    nextMark(3.6, 5.4)
 
-    nextMark(0.9, 0.9)
+    # TODO: Облет всех маркеров для поиска дронпоинтов
+    # ...
 
     home()
     land_wait()
-    file.write("green = " + str(green) + "\n")
-    file.write("yellow = " + str(yellow) + "\n")
-    file.write("red = " + str(red) + "\n")
-    file.write("blue = " + str(blue) + "\n")
-    file.close()
 
-main()
+if __name__ == '__main__':
+    main()
